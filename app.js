49c59a27861e8a89a352aef0765de65bacc46043
abduxanon <abduxanonaxmadaliyev@gmail.com>
2024-03-08 01:45:47 +0500

    fg

---
+++ app.js
@@ -0,0 +1,46 @@
+// 17-misol
+
+let ochko1 = +prompt("1 achkoni kriting");
+let ochko2 = +prompt("2 achkoni kriting");
+function ochkolar(o1, o2) {
+let result = o1 * 2 + o2 * 3;
+  return result;
+}
+console.log(ochkolar(ochko1, ochko2));
+
+//18-misol
+
+// let str1 = prompt("1 str kriting");
+// let str2 = "Edabit";
+// function string(s1, s2) {
+//   let result = s1 + s2;
+//   return result;
+// }
+// console.log(string(str1, str2));
+
+// 19-misol
+
+// let son1 = +prompt("son kriting")
+// let son2 = +prompt("2 son kriting")
+// function rost(r1,r2){
+//     let result =  r1 + r2;
+//     if(result <= 100){//100 sonidan kichik bolsa true qaytaradi
+//         return true;
+//     }else{
+//         return false;//100 sonidan kota bolsa false qaytaradi
+//     }
+// }
+// console.log(rost(son1,son2))
+
+//20-misol
+
+// function printArray(n) {
+//   const arr = [];
+//   for (let i = 1; i <= n; i++) {
+//     arr.push(i);
+//   }
+//   console.log(arr);
+// }
+// printArray(1);
+// printArray(3);
+// printArray(6);
